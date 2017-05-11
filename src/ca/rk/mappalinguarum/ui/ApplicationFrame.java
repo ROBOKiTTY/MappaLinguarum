482a1b902a6a5681462c3d4441031cc5f51b9891
ROBOKiTTY <stillwatersca@gmail.com>
2017-05-11 14:08:40 -0700

    warning message about connectivity/firewall only displays when unable to connect

--- src/ca/rk/mappalinguarum/ui/ApplicationFrame.java
+++ src/ca/rk/mappalinguarum/ui/ApplicationFrame.java
@@ -9,19 +9,28 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.SwingUtilities;
 import javax.swing.ToolTipManager;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import org.openstreetmap.gui.jmapviewer.OsmFileCacheTileLoader;
 
 /**
- * encapsulates the Swing frame component in which the application runs
+ * encapsulates the Swing frame (i.e. window) in which the application runs
  * 
  * @author RK
  *
@@ -56,6 +65,8 @@ public class ApplicationFrame extends JFrame {
 	private ApplicationMenu menuBar;
 	private JSplitPane mapAndControlSplitPane;
 	private JSplitPane topAndBottomSplitPane;
+	//placeholder panel until actual map loads
+	private JPanel mapPlaceholder;
 	private JScrollPane textPane;
 	private ControlPanel controlPanel;
 	private Map map;
@@ -64,22 +75,28 @@ public class ApplicationFrame extends JFrame {
 	 * constructs an ApplicationFrame, setting its dimensions and fields, and calls UI construction methods
 	 */
 	public ApplicationFrame() {
-		DisplayMode currentDisplay = getGraphicsConfiguration().getDevice().getDisplayMode();
-		screenWidth = currentDisplay.getWidth();
-		screenHeight = currentDisplay.getHeight();
-		appWidth = (int) (screenWidth / 1.5);
-		appHeight = (int) (screenHeight / 1.5);
-		addWindowListener( new WindowListener() );
-		addKeyListener( new KeyTroller() );
-		setSize(appWidth, appHeight);
-		setLocationRelativeTo(null);	//centres window
-		setFont(FONT);
-		setTitle(APPLICATION_TITLE);
-		setIconImage( APPLICATION_ICON.getImage() );
-		ToolTipManager.sharedInstance().setInitialDelay(TOOLTIP_INITIAL_DELAY);
-		ToolTipManager.sharedInstance().setDismissDelay(TOOLTIP_DISMISS_DELAY);
-		setVisible(true);
-
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		}
+		catch (Exception e) {
+			System.out.println("UI Look and Feel not found, defaulting to system L&F.");
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			}
+			catch (ClassNotFoundException ee) {
+				ee.printStackTrace();
+			}
+			catch (InstantiationException ee) {
+				ee.printStackTrace();
+			}
+			catch (IllegalAccessException ee) {
+				ee.printStackTrace();
+			}
+			catch (UnsupportedLookAndFeelException ee) {
+				ee.printStackTrace();
+			}
+		}
+		
 		initializeUI();
 		
 		Runnable r = new Runnable() {
@@ -98,64 +115,130 @@ public class ApplicationFrame extends JFrame {
 	 * Effect: initialize UI elements
 	 */
 	private void initializeUI() {
+		//finish building this frame
+		DisplayMode currentDisplay = getGraphicsConfiguration().getDevice().getDisplayMode();
+		screenWidth = currentDisplay.getWidth();
+		screenHeight = currentDisplay.getHeight();
+		appWidth = (int) (screenWidth / 1.5);
+		appHeight = (int) (screenHeight / 1.5);
+		addWindowListener( new WindowListener() );
+		addKeyListener( new KeyTroller() );
+		setSize(appWidth, appHeight);
+		setLocationRelativeTo(null);	//centres window
+		setFont(FONT);
+		setTitle(APPLICATION_TITLE);
+		setIconImage( APPLICATION_ICON.getImage() );
+		ToolTipManager.sharedInstance().setInitialDelay(TOOLTIP_INITIAL_DELAY);
+		ToolTipManager.sharedInstance().setDismissDelay(TOOLTIP_DISMISS_DELAY);
+		setVisible(true);
+		
 		//add bottom text console
 		TextConsole textConsole = TextConsole.getInstance();
 		textPane = textConsole.getTextPane();
 		textPane.setPreferredSize(new Dimension(appWidth , (int) (appHeight * TEXT_PANE_SIZE_MODIFIER) ) );
 		textPane.setBorder( BorderFactory.createEtchedBorder() );
-		//this.add(textPane, BorderLayout.PAGE_END);
 		
 		//add right-side panels
 		controlPanel = new ControlPanel();
 		controlPanel.setPreferredSize(new Dimension( (int) (appWidth * CONTROL_PANEL_SIZE_MODIFIER), 0) );
 		controlPanel.setBorder( BorderFactory.createEtchedBorder() );
-		//this.add(controlPanel, BorderLayout.EAST);
 
 		//add menu
 		menuBar = new ApplicationMenu(this);
 		this.setJMenuBar(menuBar);
 		
+		//create temp map placeholder
+		mapPlaceholder = new JPanel();
+		mapPlaceholder.setPreferredSize( new Dimension( (int) (appWidth * MAP_SIZE_MODIFIER), appHeight) );
+		
+		//set up split panes and add to this frame
+		mapAndControlSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, mapPlaceholder, controlPanel);
+		mapAndControlSplitPane.setDividerLocation(0.5);
+		mapAndControlSplitPane.setResizeWeight(0.5);
+		this.add(mapAndControlSplitPane);
+		
+		topAndBottomSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, mapAndControlSplitPane, textPane);
+		topAndBottomSplitPane.setDividerLocation(0.75);
+		topAndBottomSplitPane.setResizeWeight(0.75);
+		this.add(topAndBottomSplitPane);
+		
 		TextConsole.clear();
-		TextConsole.writeLine("Loading map... If the application stops responding, " +
-				"check your firewall or Internet connection.");
+		TextConsole.writeLine("Loading map...");
 
 		//force validate now that UI has been layed out
 		this.validate();
 	}
 	
 	/**
+	 * test connection to OpenStreetMap, blocking execution until connection succeeds or times out
+	 * 
+	 * @return true if connection is made
+	 */
+	private boolean testConnection() {
+		try {
+			URL url = new URL("http://tile.openstreetmap.org");
+			URLConnection conn = url.openConnection();
+			conn.connect();
+			return true;
+		}
+		//should not happen
+		catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+		catch (UnknownHostException e) {
+			TextConsole.writeLine("Error resolving hostname");
+			return false;
+		}
+		catch (SocketTimeoutException e) {
+			TextConsole.writeLine("Connection timed out.");
+			return false;
+		}
+		catch (IOException e) {
+			TextConsole.writeLine("Unspecified I/O problem. Please check error logs.");
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
 	 * initiate the map layer; this is the last stop for uncaught exceptions before main()
 	 */
 	private void loadMap() {
-		Runnable r = new Runnable() {
-			public void run() {
-				//initiate map
-				try {
-					map = new Map(controlPanel);
-					map.setPreferredSize( new Dimension( (int) (appWidth * MAP_SIZE_MODIFIER), appHeight) );
-					map.setBorder( BorderFactory.createEtchedBorder() );
-					map.setTileLoader( new OsmFileCacheTileLoader(map) );
-					mapAndControlSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, map, controlPanel);
-					mapAndControlSplitPane.setDividerLocation(0.5);
-					mapAndControlSplitPane.setResizeWeight(0.5);
-					add(mapAndControlSplitPane);
+		if (testConnection()) {
+			Runnable r = new Runnable() {
+				public void run() {
+					//initiate map
+					try {
+						map = new Map(controlPanel);
+						map.setPreferredSize( new Dimension( (int) (appWidth * MAP_SIZE_MODIFIER), appHeight) );
+						map.setBorder( BorderFactory.createEtchedBorder() );
+						map.setTileLoader( new OsmFileCacheTileLoader(map) );
+						mapAndControlSplitPane.remove(mapPlaceholder);
+						mapAndControlSplitPane.add(map);
+						controlPanel.initiateControlBoxContents();
+					}
+					catch (SecurityException e) {
+						TextConsole.writeLine("Failure to access system property for security reasons. Please check "
+								+ "error logs.");
+						e.printStackTrace();
+					}
+					catch (IOException e) {
+						TextConsole.writeLine("Cannot access map loader's system cache. Please check error logs.");
+						e.printStackTrace();
+					}
+	
+					TextConsole.writeLine("Map loaded!");
+					//force validate now that UI has been layed out
+					validate();
 				}
-				catch (Exception e) {
-					TextConsole.writeLine("Unhandled exception");
-					e.printStackTrace();
-				}
-
-				TextConsole.writeLine("Map loaded!");
-				controlPanel.initiateControlBoxContents();
-				topAndBottomSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, mapAndControlSplitPane, textPane);
-				topAndBottomSplitPane.setDividerLocation(0.75);
-				topAndBottomSplitPane.setResizeWeight(0.75);
-				add(topAndBottomSplitPane);
-				//force validate now that UI has been layed out
-				validate();
-			}
-		};
-		SwingUtilities.invokeLater(r);
+			};
+			SwingUtilities.invokeLater(r);
+		}
+		else {
+			TextConsole.writeLine("Unable to connect to OpenStreetMap. The service may be unavailable right now, "
+					+ "your Internet connectivity may be limited, or firewall settings may be blocking the connection. "
+					+ "Please restart the application when the problem is resolved.");
+		}
 	}
 	
 	//accessors
--- src/ca/rk/mappalinguarum/ui/Main.java
+++ src/ca/rk/mappalinguarum/ui/Main.java
@@ -1,9 +1,5 @@
 package ca.rk.mappalinguarum.ui;
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
 /**
  * application entry point
  * 
@@ -15,43 +11,10 @@ public class Main
 	/**
 	 * program entry point
 	 * 
-	 * effect: bootstraps the application
+	 * effect: starts the application
 	 * @param args program parameters; none is defined right now
 	 */
 	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-		}
-		catch (Exception e) {
-			System.out.println("UI Look and Feel not found, defaulting to system L&F.");
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			}
-			catch (ClassNotFoundException ee) {
-				ee.printStackTrace();
-			}
-			catch (InstantiationException ee) {
-				ee.printStackTrace();
-			}
-			catch (IllegalAccessException ee) {
-				ee.printStackTrace();
-			}
-			catch (UnsupportedLookAndFeelException ee) {
-				ee.printStackTrace();
-			}
-		}
-		
-		try {
-			Runnable r = new Runnable() {
-				public void run() {
-					new ApplicationFrame();
-				}
-			};
-			SwingUtilities.invokeLater(r);
-		}
-		catch (Throwable e) {
-			System.err.println("Unhandled error found.");
-			e.printStackTrace();
-		}
+		new ApplicationFrame();
 	}
 }
